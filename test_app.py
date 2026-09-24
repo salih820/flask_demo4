b808ebd3f4d38a8ca9e1f60a4a74abb03065e296
sali <muhammedsalih8083@gmail.com>
2026-09-24 11:23:23 +0530

    jenkins_application

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Hello, Flask!")
+
+
+if __name__ == "__main__":
+    unittest.main()
